a21c165220a60d6ae302ce078b7b761aadf66214
Bricio444 <89095958+Bricio444@users.noreply.github.com>
2024-05-07 19:14:52 +0000

    commit

--- app/src/main/java/application/controller/QuestaoController.java
+++ app/src/main/java/application/controller/QuestaoController.java
@@ -1,11 +1,14 @@
 package application.controller;
 
+import javax.swing.text.html.Option;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.saver.ResponseStatusException;
 
 import application.model.Questao;
 import application.repository.QuestaoRepository;
@@ -23,6 +26,21 @@ public class QuestaoController {
 
     @PostMapping
     public Questao post(@RequestBody Questao questao) {
+        if(!categoriaRepo.existsById(questao.getCategoria().getId())){
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,"Categoria vinculada não encontrada"
+            );
+        }
         return questoesRepo.save(questao);
     }
+    @PutMapping ("/{id}")
+    public Questao put (@RequestBody Questao questao, @PathVariable long id){
+        Option<Questao> result = questoesRepo.findbyidI(id);
+        if (result.isEmtpy()){
+            throw new ResponseStatusException(
+                HttpStatus. NOT_FOUD, "Qustão não encontrada"
+            );
+        } 
+    }
+
 }
